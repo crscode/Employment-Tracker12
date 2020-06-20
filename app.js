0ca6b272210374c6e66bf39232cec938d09dd040
Christoper Smith <Christophersmithcode@gmail.com>
2020-06-19 20:39:23 -0400

    figlet install . AcsII Art

--- app.js
+++ app.js
@@ -469,3 +469,29 @@ function runApp() {
     });
 }
 
+var figlet = require('figlet');
+ 
+figlet('EMPLOYMENT TRACKER!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+	console.log(data);
+	
+});
+
+figlet.text('CRSCODE!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+	console.log(data);
+	runApp();
+	
+});
